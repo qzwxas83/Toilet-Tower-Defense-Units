d351cd71f91391aa59aec749260685bcc157cb21
qzwxas83 <132167934+qzwxas83@users.noreply.github.com>
2024-05-28 21:24:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // Event listener for theme toggle button
     themeToggleBtn.addEventListener('click', toggleTheme);
 });
+
+function scrollToSection(sectionId) {
+    var element = document.getElementById(sectionId);
+    if (element) {
+        var headerHeight = document.querySelector('header').offsetHeight;
+        var topMargin = 20;
+        var scrollPosition = element.offsetTop - headerHeight - topMargin;
+        
+        if (window.scrollY !== scrollPosition) {
+            window.scrollTo({
+                top: scrollPosition,
+                behavior: 'smooth'
+            });
+        }
+    }
+}
